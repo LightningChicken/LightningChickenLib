7160378a530c0d4029e4c4f25424325e626656fc
toliner <toliner.rkt@gmail.com>
2017-11-04 20:30:00 +0900

    IRBDSerializableにドキュメントを追加

--- src/main/kotlin/lightningchicken/lib/rbd/IRBDSerializable.kt
+++ src/main/kotlin/lightningchicken/lib/rbd/IRBDSerializable.kt
@@ -2,9 +2,24 @@ package lightningchicken.lib.rbd
 
 import raiti.RaitisAPI.DataOperation.RBD.RBD
 
+/**
+ * RBDに変換可能であることを表すインターフェースです。
+ * このインターフェースが実装されているクラスは、全てRBDとの相互変換が可能である必要があります。
+ * @author toliner
+ * @param T このインターフェースを継承しているクラスの型
+ */
 interface IRBDSerializable<out T> {
 
+    /**
+     * オブジェクトをRBDに変換します。
+     * @return RBDに変換されたオブジェクトのデータ
+     */
     fun serialize(): RBD
 
+    /**
+     * RBDをオブジェクトに変換します。
+     * @param data 対象となるRBD
+     * @return 変換後のこのオブジェクト
+     */
     fun deserialize(data: RBD): T
 }
