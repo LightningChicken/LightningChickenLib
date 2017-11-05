50409a1159b76a13d747af4ff16438086939506f
toliner <toliner.rkt@gmail.com>
2017-11-05 13:56:34 +0900

    addAllのうち、STDLibにない方を再実装

--- src/main/kotlin/lightningchicken/lib/arrays/ArraysUtil.kt
+++ src/main/kotlin/lightningchicken/lib/arrays/ArraysUtil.kt
@@ -1,3 +1,82 @@
 package lightningchicken.lib.arrays
 
+import raitichan.raitisapi.arrays.ArraysUtility
 
+//=================================================================================================
+//---------------- addAll
+/**
+ * 指定した配列を全て1つの配列にまとめます。<br>
+ *
+ * @param addArrays 追加する配列
+ * @return 追加後の配列
+ */
+fun ByteArray.addAll(vararg addArrays: ByteArray): ByteArray {
+    addArrays.forEach { ArraysUtility.addAll(this, it) }
+    return this
+}
+
+/**
+ * 指定した配列を全て1つの配列にまとめます。<br>
+ *
+ * @param addArrays 追加する配列
+ * @return 追加後の配列
+ */
+fun ShortArray.addAll(vararg addArrays: ShortArray): ShortArray {
+    addArrays.forEach { ArraysUtility.addAll(this, it) }
+    return this
+}
+
+/**
+ * 指定した配列を全て1つの配列にまとめます。<br>
+ *
+ * @param addArrays 追加する配列
+ * @return 追加後の配列
+ */
+fun IntArray.addAll(vararg addArrays: IntArray): IntArray {
+    addArrays.forEach { ArraysUtility.addAll(this, it) }
+    return this
+}
+
+/**
+ * 指定した配列を全て1つの配列にまとめます。<br>
+ *
+ * @param addArrays 追加する配列
+ * @return 追加後の配列
+ */
+fun LongArray.addAll(vararg addArrays: LongArray): LongArray {
+    addArrays.forEach { ArraysUtility.addAll(this, it) }
+    return this
+}
+
+/**
+ * 指定した配列を全て1つの配列にまとめます。<br>
+ *
+ * @param addArrays 追加する配列
+ * @return 追加後の配列
+ */
+fun FloatArray.addAll(vararg addArrays: FloatArray): FloatArray {
+    addArrays.forEach { ArraysUtility.addAll(this, it) }
+    return this
+}
+
+/**
+ * 指定した配列を全て1つの配列にまとめます。<br>
+ *
+ * @param addArrays 追加する配列
+ * @return 追加後の配列
+ */
+fun DoubleArray.addAll(vararg addArrays: DoubleArray): DoubleArray {
+    addArrays.forEach { ArraysUtility.addAll(this, it) }
+    return this
+}
+
+/**
+ * 指定した配列を全て1つの配列にまとめます。<br>
+ *
+ * @param addArrays 追加する配列
+ * @return 追加後の配列
+ */
+fun BooleanArray.addAll(vararg addArrays: BooleanArray): BooleanArray {
+    addArrays.forEach { ArraysUtility.addAll(this, it) }
+    return this
+}
